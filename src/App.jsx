a038b1fbd7e65da2b48d2f417332797872c8ec8f
Moises Romero <mromero2100@gmail.com>
2022-12-28 15:52:42 -0600

    adding StrictMode

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
-import Kanban from './components/kanban/Kanban'
+import Kanban from './components/kanban/Kanban';
 
 function App() {
   return (
     <div style={{ padding: '50px' }}>
-      <h1 style={{ marginBottom: '20px' }}>Kanban Dashboard</h1>
+      <h1 style={{ marginBottom: '20px' }}>
+        Kanban Dashboard
+      </h1>
       <Kanban />
     </div>
   )
